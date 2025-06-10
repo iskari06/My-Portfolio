9d6dacdd713a18218f176fc7c0636210ea113c93
Christopher Lazaga <christopherlazaga02@gmail.com>
2025-06-10 17:32:44 +0800

    plsplsplspls

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -8,6 +8,8 @@ import {
 	MotionValue,
 } from "framer-motion";
 
+import resumePdf from "/resume.pdf";
+
 export const HeroParallax = ({
 	products,
 }: {
@@ -113,12 +115,12 @@ export const Header = () => {
 						GitHub Profile
 					</span>
 				</a>
-        <a 
-          href="/resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-        >
+				<a
+					href={resumePdf}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+				>
 					<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 					<span className="inline-flex h-full w-full z-10 cursor-pointer items-center justify-center rounded-full bg-slate-950/80 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
 						<FileTextOutlined className="mr-2" />
